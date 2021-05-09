38a1080dd5d07b3795c16c179598bc66dc15aef0
Andrey NESTEROV <andrey.nesterov@laposte.net>
2021-05-09 15:42:13 +0200

    config.php and index.php were created, the host is checked

--- classes/config.php
+++ classes/config.php
@@ -1,2 +1,21 @@
 <?php
-require_once 'core/init.php';
+class Config
+{
+    //by default null, in order to verify if my $path config exists
+    public static function get($path=null)
+    {
+        if ($path) {
+            $config = $GLOBALS ['config'];
+            $path = explode('/', $path);
+        
+            // to see if we have access to our host 127.0.0.1
+            foreach ($path as $bit) {
+                if (isset($config[$bit])) {
+                    $config = $config[$bit];
+                }
+            }
+            return $config;
+        }
+    return false;
+    }
+}
--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php
 require_once 'core/init.php';
+
+//if i have access it should print number of my host
+echo Config::get('mysql/host'); // 127.0.0.1
